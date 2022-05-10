4508541f4b995001ee3fedc644269ef142c950fb
0xstu <39035663+0xstu@users.noreply.github.com>
2022-05-10 10:37:10 +0200

    added smart contract code for reference

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 import { shortenAddress } from '../utils/shortenAddress';
 
-const TransacationCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+const TransacationCard = ({ addressTo, addressFrom, timestamp, message, amount }) => {
 
   return ( <div className='bg-[#181918] m-4 flex flex-1
   2xl:min-w-[450px]
@@ -14,10 +14,10 @@ const TransacationCard = ({ addressTo, addressFrom, timestamp, message, keyword,
     <div className='flex flex-col items-center w-full mt-3'>
       <div className='w-full mb-6 p-2'>
         <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target='blank' rel="noopener noreferrer">
-          <p className='text-white text-base'>From: {shortenAddress(addressFrom)}</p>
+          <p className='text-white text-base font-bold hover:text-[#2138D1]'>From: {shortenAddress(addressFrom)}</p>
         </a>
         <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target='blank' rel="noopener noreferrer">
-          <p className='text-white text-base'>To: {shortenAddress(addressTo)}</p>
+          <p className='text-white text-base font-bold hover:text-[#2138D1]'>To: {shortenAddress(addressTo)}</p>
         </a>
         <p className='text-white text-base'>Amount: {amount} ETH</p>
         {message && (
@@ -29,7 +29,9 @@ const TransacationCard = ({ addressTo, addressFrom, timestamp, message, keyword,
 
       </div>
           <div className='bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl'>
+          <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target='blank' rel="noopener noreferrer">
             <p className='text-[#37c7da] font-bold'>{timestamp}</p>
+            </a>
           </div>
     </div>
   </div>
@@ -46,7 +48,7 @@ const Transactions = () => {
       {currentAccount ? (
         <h3 className='text-white text-3xl text-center my-2'>Latest Transactions</h3>
       ) : (
-        <h3 className='text-white text-3xl text-center my-2'>Connect your account to see your transactions</h3>
+        <h3 className='text-white text-3xl text-center my-2'>Connect your wallet to see your transactions</h3>
       )}
       <div className='flex flex-wrap justify-center items-center mt-10'>
         {transactions.reverse().map((transaction, i)=>(
--- src/context/TransactionContext.jsx
+++ src/context/TransactionContext.jsx
@@ -11,32 +11,24 @@ const getEthereumContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
-
+    console.log(ethereum);
     return transactionContract;
 }
 
 export const TransactionProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState('')
-    const [isLoading, setIsLoading] = useState(false);
-    //const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'));
     const [transactions, setTransactions] = useState([]);
-
-    const handleChange = (e, name) => {
-        setFormData((prevState) => ({...prevState, [name]: e.target.value}))
-    }
 
     const getAllTransactions = async () => {
         try {
             if(!ethereum) return alert('Please install MetaMask');
             const transactionContract = getEthereumContract();
             const availableTransactions = await transactionContract.getAllTransactions();
-
             const structuredTransactions = availableTransactions.map((transaction) => ({
                 addressTo: transaction.receiver,
                 addressFrom: transaction.sender,
                 timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
                 message: transaction.message,
-                keyword: transaction.keyword,
                 amount: parseInt(transaction.amount._hex) / (10 ** 18)
             }))
             setTransactions(structuredTransactions);
@@ -83,7 +75,7 @@ export const TransactionProvider = ({ children }) => {
     }, []);
 
     return (
-        <TransactionContext.Provider value={{ connectWallet, currentAccount, handleChange, transactions, isLoading }}>
+        <TransactionContext.Provider value={{ connectWallet, currentAccount, transactions }}>
             {children}
         </TransactionContext.Provider>
     );
